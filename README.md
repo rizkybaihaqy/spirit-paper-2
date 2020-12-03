19dc8773e849ab22924d74fb45afc87fe083d23b
rizkybaihaqy <rizkybaihaqy123@gmail.com>
2020-12-03 14:59:58 +0700

    Refactoring and commenting a lot of code

--- README.md
+++ README.md
@@ -1,3 +1,92 @@
 # spirit-paper-2
 
-biar gak capek ngedit
+Spirit-paper-2 adalah sebuah aplikasi berbasis web untuk pembuatan spirit paper oleh
+Staff ahli LITBANG HMIF UNDIP 2020. Aplikasi ini hanya bekerja pada client side.
+Aplikasi ini memanfaatkan tag canvas html untuk merender dan mengedit gambar.
+
+spirit paper 2 adalah pengembangan dari prototype spirit paper
+(https://github.com/rizkybaihaqy/spirit-paper)(https://rizkybaihaqy.github.io/spirit-paper)
+
+Aplikasi dibuat menggunakan javascript dengan framework Fabric.js (http://fabricjs.com),
+FileSaver.js (https://github.com/eligrey/FileSaver.js) dan jquery (https://jquery.com/).
+
+    Farbic.js digunakan untuk memudahkan pembuatan object pada <canvas>. Ketergantungan
+    program ini pada Fabric.js sangatlah besar karena fitur penting pada aplikasi dibangun
+    menggunakan framework ini. versi yang diganakan saat ini adalah 4.1.0
+
+    FileSaver.js digunakan untuk menyimpan tampilan <canvas> yang telah di render kedalam
+    sebuah file gambar. versi yang diganakan saat ini adalah 2.0.2
+
+    jQuery digunakan sebagai pelengkap untuk mempermudah penulisan javascript. versi yang
+    diganakan saat ini adalah 3.2.1 slim
+
+Styling aplikasi menggunakan framework css material design lite (https://getmdl.io/).
+class dapat dikenali dengan prefix mdl. versi yang dignakan saat ini adalah 1.3.0
+Icon yang digunakan pada aplikasi adalah material icons (https://material.io/resources/icons).
+
+Telah dilakukan langkah awal untuk mengikuti istilah progresive web app (pwa). icon beragam
+ukuran dan manifest.json telah dibuat. hal selanjutnya yang perlu dilakukan adalah membuat
+service worker dan pengaturan caching.
+
+Aplikasi dibagi menjadi 3 tab. simple, multi, custom.
+Ukuran canvas disetiap tab selalu 500x500 px
+
+Pada setiap Tab memiliki kolom teks meminta tujuan surat yang
+nantinya isi dari kolom teks ini akan digunakan untuk menamai
+nama file yang akan di download.
+
+Pada setiap tab juga memiliki tombol DOWNLOAD. tombol ini digunakan
+untuk mendownload tamapilan canvas saat ini kedalam file gambar.
+
+Hanya ada satu validasi yg dilakukan saat tombol download di click
+yaitu mengecek apakah tujuan surat sudah diisi. Hal ini dilakukan
+karena tujuan surat akan digunakan sebagai nama file gamabar dan
+tidak boleh kosong.
+
+Object pada canvas dapat di hapus menggunakan key delete pada
+keyboard (tidak terdapat di tab simple)
+
+Tab Pertama simple canvas
+
+    Simple memiliki fitur yang paling sedikit dan paling sederhana.
+
+    Pengguna dapat memilih SATU gambar makanan sebagai backgroud dari
+    pilihan gambar yang telah disediakan. Gamabr yang telah disediakn
+    juga sudah memiliki memo kosong yang siap diisi. posisi memo SUDAH
+    ditentukan.
+
+    Pengguna dapat mengisi SATU teksbox untuk dituliskan pesan.
+
+Tab Kedua Multi teksbox
+
+    Multi memiliki fitur untuk menambahkan teksbox.
+
+    Pengguna dapat memilih SATU gambar makanan sebagai backgroud dari
+    pilihan gambar yang telah disediakan. Gamabr yang telah disediakn
+    juga sudah memiliki memo kosong yang siap diisi. posisi memo SUDAH
+    ditentukan. Gamabar yang dignakan pada tab ini memiliki DUA memo
+    atau lebih.
+
+    Pengguna dapat mengisi LEBIH DARI SATU teksbox untuk dituliskan pesan.
+    pengguna dapaet menggunakan tombol TAMBAH TEKSBOX untuk menambah obejct
+    teksbox ke dalam canvas yang nantinya dapat dituliskan pesan.
+
+    Pada tab Multi setiap id yang digunakan hampir sama dengan yang digunakan
+    di tab simple. Pembedanya adalah penggunaan suffix -multi
+
+Tab Kedua Custom image
+
+    Custom memiliki fitur untuk menambahkan gambar sendiri.
+
+    Pengguna dapat mengUPLOAD gambarnya sendiri. Ukuran Gambar yang dianjurkan
+    adalah 500x500 px. Gambar tidak wajib memiliki memo.
+
+    Pengguna dapat mengisi LEBIH DARI SATU teksbox untuk dituliskan pesan.
+    pengguna dapaet menggunakan tombol TAMBAH TEKSBOX untuk menambah obejct
+    teksbox ke dalam canvas yang nantinya dapat dituliskan pesan.
+
+    Pada tab Custom memo diberikan bersamaan dengan teksbox. posisi memo
+    dapat DIUBAH.
+
+    Pada tab Custom setiap id yang digunakan hampir sama dengan yang digunakan
+    di tab simple. Pembedanya adalah penggunaan suffix -custom
--- js/raport.js
+++ js/raport.js
@@ -1,39 +1,11 @@
+/*
+ * KAMUS GLOBAL
+ */
 var img = new Image();
 var mobileZoom = 0.61;
+var added;
 
-// (function () {
-fabric.Object.prototype.originX = fabric.Object.prototype.originY = "center";
-
-var canvas = (this.__canvas = new fabric.Canvas("c", {
-  preserveObjectStacking: true,
-}));
-canvas.setBackgroundImage(
-  "../imgs/raport/595x842.png",
-  canvas.renderAll.bind(canvas),
-  {
-    originX: "left",
-    originY: "top",
-  }
-);
-if (screen.width < 576) {
-  canvas.setZoom(mobileZoom);
-  canvas.setWidth(canvas.width * canvas.getZoom());
-  canvas.setHeight(canvas.height * canvas.getZoom());
-}
-$("#mobileZoomOut").on("click", function () {
-  mobileZoom = mobileZoom - 0.01;
-  canvas.setZoom(mobileZoom);
-  canvas.setWidth(595 * canvas.getZoom());
-  canvas.setHeight(842 * canvas.getZoom());
-  console.log(canvas.width);
-});
-$("#mobileZoomIn").on("click", function () {
-  mobileZoom = mobileZoom + 0.01;
-  canvas.setZoom(mobileZoom);
-  canvas.setWidth(595 * canvas.getZoom());
-  canvas.setHeight(842 * canvas.getZoom());
-});
-
+//teks pesan
 var textOptions = {
   width: 480,
   fontSize: 15,
@@ -47,16 +19,80 @@ var textOptions = {
   lockScalingY: true,
   lockScalingFlip: true,
 };
-var isiNamaTeks;
-var isiJabatanTeks;
+// graph rating
 var warnaDivisi = "rgba(255,0,0,0.5)";
-var added;
+var modifier = 4;
+var rectOptions = {
+  width: 75,
+  height: 100 * modifier,
+  top: 775,
+  left: 150,
+  fill: warnaDivisi,
+  selectable: false,
+  rx: 10,
+};
 
+// graph teks
+var isiTeksNilai1 = "Komitmen";
+var isiTeksNilai2 = "Komunikasi";
+var isiTeksNilai3 = "Inisiatif";
+var isiTeksNilai4 = "Bidang";
+var teksNilaiOptions = {
+  width: 200,
+  fontSize: 20,
+  fontFamily: "Helvetica",
+  fill: "white",
+  left: 100,
+  top: 775,
+  textAlign: "center",
+  selectable: false,
+};
+/*
+ *
+ */
+fabric.Object.prototype.originX = fabric.Object.prototype.originY = "center";
+
+/*
+ * Inisialisasi
+ * Di lakukan pembuatan instance canvas
+ */
+var canvas = (this.__canvas = new fabric.Canvas("c", {
+  preserveObjectStacking: true,
+}));
+
+/*
+ * Menset backgroud image dan kemudian merendernya
+ * Background image diposisikan ditengah canvas
+ */
+canvas.setBackgroundImage(
+  "../imgs/raport/595x842.png",
+  canvas.renderAll.bind(canvas),
+  {
+    originX: "left",
+    originY: "top",
+  }
+);
+
+/*
+ * Pada layar kecil / mobile isi dari canvas akan di zoomout
+ * Nilai default adalah 0.61,
+ * nilai lebih kecil dari 1 melakukan zoom out
+ * Ukuran canvas juga akan dikali dengan getZoom() yang
+ * mengembalikan nilai Zoom atau dalam hal ini 0.61
+ */
+if (screen.width < 576) {
+  canvas.setZoom(mobileZoom);
+  canvas.setWidth(canvas.width * canvas.getZoom());
+  canvas.setHeight(canvas.height * canvas.getZoom());
+}
+
+// Pembuatan instance text pesan
 var text = new fabric.Textbox(
   "Kalo masih ada yang mau disampein dan udah gamuat, kalian bisa pakek yang satunya lagi yang ada gamabar makannanya. Teks box ini Bisa di atur posisinya dengan cara pilh teksbox lalu geser. Cara edit teksnya adalah dengan select tekxbox hingga dapat mengedit teks. ",
   textOptions
 );
 
+// Pembuatan instance namaTeks (teks nama pada raport)
 var namaTeks = new fabric.Textbox("Nama", textOptions);
 namaTeks.set({
   fontSize: 20,
@@ -65,6 +101,8 @@ namaTeks.set({
   top: 235,
   selectable: false,
 });
+
+// Pembuatan instance jabatanTeks (teks jabatan pada raport)
 var jabatanTeks = new fabric.Textbox("Jabatan", textOptions);
 jabatanTeks.set({
   fontSize: 25,
@@ -73,17 +111,88 @@ jabatanTeks.set({
   top: 280,
   selectable: false,
 });
-// canvas.add(namaTeks);
-// canvas.add(jabatanTeks);
-// canvas.add(text);
 
-$("#nama").on("change", function (isiNamaTeks) {
-  isiNamaTeks = String(this.value);
+// Pembuatan instance graph rating nilai 1-4
+var nilai1 = new fabric.Rect(rectOptions);
+nilai1.set("left", 100);
+var nilai2 = new fabric.Rect(rectOptions);
+nilai2.set("left", 235);
+var nilai3 = new fabric.Rect(rectOptions);
+nilai3.set("left", 365);
+var nilai4 = new fabric.Rect(rectOptions);
+nilai4.set("left", 500);
+
+// Pembuatan instance clipPath (bentuk dari clip) untuk graph rating
+var clipPath = new fabric.Rect({
+  width: 500,
+  height: 250,
+  top: 650,
+  left: 300,
+  absolutePositioned: true,
+});
+
+// Pembuatan instance group dari nilai 1-4
+var group = new fabric.Group([nilai1, nilai2, nilai3, nilai4]);
+group.set({
+  selectable: false,
+});
+// Menlipping group
+group.clipPath = clipPath;
+
+// Pembuatan instance teks pada graph rating nilai 1-4
+var teksNilai1 = new fabric.Textbox("100\n" + isiTeksNilai1, teksNilaiOptions);
+teksNilai1.set("left", 100);
+var teksNilai2 = new fabric.Textbox("100\n" + isiTeksNilai2, teksNilaiOptions);
+teksNilai2.set("left", 235);
+var teksNilai3 = new fabric.Textbox("100\n" + isiTeksNilai3, teksNilaiOptions);
+teksNilai3.set("left", 365);
+var teksNilai4 = new fabric.Textbox("100\n" + isiTeksNilai4, teksNilaiOptions);
+teksNilai4.set("left", 500);
+/*
+ * endOfInisialisazi
+ */
+
+/*
+ * Prosedur zoomOut dan zoomIn memiliki cara kerja yang sama
+ * perbedaannya adalah dilakukannya penambahan/pengurangan
+ * nilai zoom pada prosedur ini
+ */
+function zoomOut() {
+  mobileZoom = mobileZoom - 0.01;
+  canvas.setZoom(mobileZoom);
+  canvas.setWidth(595 * canvas.getZoom());
+  canvas.setHeight(842 * canvas.getZoom());
+}
+function zoomIn() {
+  mobileZoom = mobileZoom + 0.01;
+  canvas.setZoom(mobileZoom);
+  canvas.setWidth(595 * canvas.getZoom());
+  canvas.setHeight(842 * canvas.getZoom());
+}
+
+/*
+ * Prosedur printNama
+ * Mengambil nilai dari #nama dan kemudian di masukan
+ * ke properti dari instance namaTeks dan di render
+ */
+function printNama() {
+  var isiNamaTeks = $("#nama").val();
   namaTeks.text = isiNamaTeks;
   canvas.requestRenderAll();
-});
-$("#jabatan").on("change", function (isiJabatanTeks) {
-  isiJabatanTeks = String(this.value);
+}
+
+/*
+ * Prosedur pilihJabatan
+ * Mengambil nilai dari #jabatan dan kemudian di masukan
+ * ke properti dari instance namaTeks.
+ * Tapi bukan hanya itu.
+ * Prosedur ini cukup complex, seharusnya dapat dipecah menjadi modeul yang lebih kecil
+ * Prosedur ini juga memilih background image dan colorFill dari setiap divisi.
+ * Gamabar di load menggunakan static method fromURL() dan diberi filter pengurangan brightness
+ * Terahkir, dilakukan penambahan selujurh object ke dalam canvas pada run pertama kali.
+ */
+function pilihJabatan() {
+  var isiJabatanTeks = $("#jabatan").val();
   jabatanTeks.text = isiJabatanTeks;
 
   var gambar;
@@ -138,9 +247,7 @@ $("#jabatan").on("change", function (isiJabatanTeks) {
   nilai3.set("fill", warnaDivisi);
   nilai4.set("fill", warnaDivisi);
 
-  img.src = gambar;
-
-  fabric.Image.fromURL(img.src, function (img) {
+  fabric.Image.fromURL(gambar, function (img) {
     img.set({
       // Needed to position backgroundImage at 0/,
       originX: "left",
@@ -153,9 +260,9 @@ $("#jabatan").on("change", function (isiJabatanTeks) {
     img.filters.push(filter);
     img.applyFilters();
 
-    canvas.backgroundImage = img;
-    // canvas.add(img);
+    canvas.setBackgroundImage(img, canvas.renderAll.bind(canvas));
 
+    // Hanya jalan sekali
     if (Boolean(added) == false) {
       canvas.add(namaTeks);
       canvas.add(jabatanTeks);
@@ -172,33 +279,159 @@ $("#jabatan").on("change", function (isiJabatanTeks) {
     }
 
     canvas.requestRenderAll();
-    canvas.bringToFront(text);
   });
-});
+}
 
-// function addTextBox() {
-//   var textLiar = new fabric.Textbox("TEXTBOX TAMBAHAN", {
-//     width: 200,
-//     fontSize: 20,
-//     fontFamily: "Helvetica",
-//     fill: "white",
-//     left: 250,
-//     top: 260,
-//     textAlign: "center",
-//   });
-//   canvas.add(textLiar);
-//   canvas.bringToFront(textLiar);
-//   console.log("ada");
-// }
-// })();
+/*
+ * Prosedur uploadGamabr()
+ * Akan mnegecek apakah ada perubahan pada input #pilihanGambar-custom
+ * Kemudian upload gmabar dan
+ * (https://stackoverflow.com/questions/34945342/remove-drag-property-from-uploaded-image-in-canvas-html5-set-area-to-uploaded-i)
+ * Setelah gambar di upload di clipping ke dalam clippath berbentuk lingkaran
+ * clippath tidak bisa digerakan user
+ * user dapat menggerakan foto yang telah di upload
+ */
+function uploadGambar() {
+  $("#profil").on("change", function (e) {
+    var file = e.target.files[0];
+    var reader = new FileReader();
+    reader.onload = function (f) {
+      var data = f.target.result;
+      fabric.Image.fromURL(data, function (img) {
+        img.scale(0.2).set({
+          left: 475,
+          top: 200,
+          clipPath: new fabric.Circle({
+            left: 475,
+            top: 150,
+            radius: 79,
+            absolutePositioned: true,
+          }),
+        });
+        canvas.add(img);
+      });
+    };
+    reader.readAsDataURL(file);
+  });
+}
 
-document.getElementById("nama").addEventListener("change", function () {
-  var nama = document.getElementById("nama").value;
-  nama = nama + ".png";
-  $("#download").click(function () {
+/*
+ * Prosedur kelasWarnaAbu
+ * Menghilangkan kelas mdl-color-text--blue pada element dan
+ * Memberikan kelas mdl-color-text--grey-400 pada elemnt
+ */
+function kelasWarnaAbu(i) {
+  $(".label-slider")
+    .eq(i)
+    .removeClass("mdl-color-text--blue")
+    .addClass("mdl-color-text--grey-400");
+}
+
+/*
+ * Prosedur kelasWarnaBiru
+ * Menghilangkan kelas mdl-color-text--grey-400 pada element dan
+ * Memberikan kelas mdl-color-text--blue pada elemnt
+ */
+function kelasWarnaBiru(i) {
+  $(".label-slider")
+    .eq(i)
+    .removeClass("mdl-color-text--grey-400")
+    .addClass("mdl-color-text--blue");
+}
+
+/*
+ * Prosedur graphNilai1-4
+ * menjalankan prosedur kelasWarnaBiru pada index kelas yang dituju (mulai dari 0)
+ * Menset nilai properti height dari instance nilai1-4 dengan value dari #nilai1 * modifier
+ * Penggunaan modifier adalah untuk menyesuaikan dengan ukuran canvas. Karena value dari
+ * #nilai hanya 0-100 maka perlu dikali dengan modifier (default 4).
+ * Selanjutnya menset nilai dari properti text dari instance teksnilai1-4 dengan value
+ * dari #nilai + nama/label dari nilai tersebut dan tidak lupa di render.
+ * menjalankan prosedur kelasWarnaBiru pada index kelas yang dituju (mulai dari 0) pada saat
+ * mouse atau touch selesai dengan element
+ */
+function graphNilai1() {
+  kelasWarnaBiru(0);
+  nilai1.set("height", parseInt($("#nilai1").val()) * modifier);
+  teksNilai1.text = $("#nilai1").val() + "\n" + isiTeksNilai1;
+  canvas.requestRenderAll();
+  $("#nilai1").on("mouseup touchend", function () {
+    kelasWarnaAbu(0);
+  });
+}
+function graphNilai2() {
+  kelasWarnaBiru(1);
+  nilai2.set("height", parseInt($("#nilai2").val()) * modifier);
+  teksNilai2.text = $("#nilai2").val() + "\n" + isiTeksNilai2;
+  canvas.requestRenderAll();
+  $("#nilai2").on("mouseup touchend", function () {
+    kelasWarnaAbu(1);
+  });
+}
+function graphNilai3() {
+  kelasWarnaBiru(2);
+  nilai3.set("height", parseInt($("#nilai3").val()) * modifier);
+  teksNilai3.text = $("#nilai3").val() + "\n" + isiTeksNilai3;
+  canvas.requestRenderAll();
+  $("#nilai3").on("mouseup touchend", function () {
+    kelasWarnaAbu(2);
+  });
+}
+function graphNilai4() {
+  kelasWarnaBiru(3);
+  nilai4.set("height", parseInt($("#nilai4").val()) * modifier);
+  teksNilai4.text = $("#nilai4").val() + "\n" + isiTeksNilai4;
+  canvas.requestRenderAll();
+  $("#nilai4").on("mouseup touchend", function () {
+    kelasWarnaAbu(3);
+  });
+}
+
+/*
+ * Fungsi getNama.
+ * Fungsi akan mengambil dan mengembalikan nilai dari #nama
+ * ditambah dengan .png
+ * (hanya untuk mempermudah dalam membuka file bukan untuk menentukan ekstensi file)
+ * Jika kosong akan memanggil fungsi snackbar
+ * dan mengembalikan nilai false
+ */
+function getNama() {
+  if ($("#nama").val() == "") {
+    fireSnackBar();
+    return false;
+  } else {
+    var nama = $("#nama").val();
+    nama = nama + ".png";
+    return nama;
+  }
+}
+
+/*
+ * Prosedur untuk memunculkan snackbar
+ * Sudah bawaan dari mdl
+ */
+function fireSnackBar() {
+  document.querySelector(".mdl-js-snackbar").MaterialSnackbar.showSnackbar({
+    message: "Tulis Tujuan Surat Terlebih Dahulu",
+  });
+}
+
+/*
+ * Prosedur Print
+ * Mengecek apakah fungsi getNama() mengembalikan nilai true
+ * Kemudian mengambil kembalian dari fungsi getNama()
+ * Sebelum print dilakukan pembersihan object yang aktif agar kontrol pembantunya tidak ikut di cetak.
+ * Timeout Prosedur untuk menuggu pemberisah object aktif. hal ini dilakukan karena sifat alami js yg async
+ * Printing dilakukan dengan mengambil isi dari #c dan kemudian menkonvernya menjadi blob
+ * Setelah itu dilakukan Prosedur saveAs() dari FileSaver.js
+ */
+function print() {
+  if (getNama()) {
+    nama = getNama();
     //bersihkan canvas sebelum print
     canvas.discardActiveObject(canvas.getActiveObject());
     canvas.requestRenderAll();
+    //timeout menuggu canvas dibersihkan
     setTimeout(function () {
       //printing
       $("#c")
@@ -207,172 +440,14 @@ document.getElementById("nama").addEventListener("change", function () {
           saveAs(blob, nama);
         });
     }, 1000);
-  });
-});
-
-function validasiForm() {
-  if (document.forms["spiritPaper"]["nama"].value == "") {
-    document.querySelector(".mdl-js-snackbar").MaterialSnackbar.showSnackbar({
-      message: "Tulis Tujuan Surat Terlebih Dahulu",
-    });
-    return false;
-  } else {
-    return true;
   }
 }
 
-// foto
-document.getElementById("profil").addEventListener("change", function (e) {
-  var file = e.target.files[0];
-  var reader = new FileReader();
-  reader.onload = function (f) {
-    var data = f.target.result;
-    fabric.Image.fromURL(data, function (img) {
-      img.scale(0.2).set({
-        left: 475,
-        top: 200,
-        clipPath: new fabric.Circle({
-          left: 475,
-          top: 150,
-          radius: 79,
-          absolutePositioned: true,
-        }),
-      });
-      canvas.add(img);
-    });
-  };
-  reader.readAsDataURL(file);
-});
-
-//Grafik Nilai
-
-var modifier = 4;
-
-var rectOptions = {
-  width: 75,
-  height: 100 * modifier,
-  top: 775,
-  left: 150,
-  fill: warnaDivisi,
-  selectable: false,
-  rx: 10,
-};
-
-var nilai1 = new fabric.Rect(rectOptions);
-nilai1.set("left", 100);
-// canvas.add(nilai1);
-var nilai2 = new fabric.Rect(rectOptions);
-nilai2.set("left", 235);
-// canvas.add(nilai2);
-var nilai3 = new fabric.Rect(rectOptions);
-nilai3.set("left", 365);
-// canvas.add(nilai3);
-var nilai4 = new fabric.Rect(rectOptions);
-nilai4.set("left", 500);
-// canvas.add(nilai3);
-
-// group clipping
-var clipPath = new fabric.Rect({
-  width: 500,
-  height: 250,
-  top: 650,
-  left: 300,
-  absolutePositioned: true,
-});
-// canvas.add(clipPath);
-var group = new fabric.Group([nilai1, nilai2, nilai3, nilai4]);
-group.set({
-  selectable: false,
-});
-group.clipPath = clipPath;
-// canvas.add(group);
-
-// Nilai
-
-var isiTeksNilai1 = "Komitmen";
-var isiTeksNilai2 = "Komunikasi";
-var isiTeksNilai3 = "Inisiatif";
-var isiTeksNilai4 = "Bidang";
-
-var teksNilaiOptions = {
-  width: 200,
-  fontSize: 20,
-  fontFamily: "Helvetica",
-  fill: "white",
-  left: 100,
-  top: 775,
-  textAlign: "center",
-  selectable: false,
-};
-
-var teksNilai1 = new fabric.Textbox("100\n" + isiTeksNilai1, teksNilaiOptions);
-teksNilai1.set("left", 100);
-// canvas.add(teksNilai1);
-var teksNilai2 = new fabric.Textbox("100\n" + isiTeksNilai2, teksNilaiOptions);
-teksNilai2.set("left", 235);
-// canvas.add(teksNilai2);
-var teksNilai3 = new fabric.Textbox("100\n" + isiTeksNilai3, teksNilaiOptions);
-teksNilai3.set("left", 365);
-// canvas.add(teksNilai3);
-var teksNilai4 = new fabric.Textbox("100\n" + isiTeksNilai4, teksNilaiOptions);
-teksNilai4.set("left", 500);
-// canvas.add(teksNilai4);
-
-$("#nilai1").on("input", function () {
-  kelasWarnaBiru(0);
-  nilai1.set("height", parseInt(this.value) * modifier);
-  teksNilai1.text = String(this.value) + "\n" + isiTeksNilai1;
-  canvas.requestRenderAll();
-  $("#nilai1").on("mouseup touchend", function () {
-    kelasWarnaAbu(0);
-  });
-});
-$("#nilai2").on("input", function () {
-  kelasWarnaBiru(1);
-  nilai2.set("height", parseInt(this.value) * modifier);
-  teksNilai2.text = String(this.value) + "\n" + isiTeksNilai2;
-  canvas.requestRenderAll();
-  $("#nilai2").on("mouseup touchend", function () {
-    kelasWarnaAbu(1);
-  });
-});
-$("#nilai3").on("input", function () {
-  kelasWarnaBiru(2);
-  nilai3.set("height", parseInt(this.value) * modifier);
-  teksNilai3.text = String(this.value) + "\n" + isiTeksNilai3;
-  canvas.requestRenderAll();
-  $("#nilai3").on("mouseup touchend", function () {
-    kelasWarnaAbu(2);
-  });
-});
-$("#nilai4").on("input", function () {
-  kelasWarnaBiru(3);
-  nilai4.set("height", parseInt(this.value) * modifier);
-  teksNilai4.text = String(this.value) + "\n" + isiTeksNilai4;
-  canvas.requestRenderAll();
-  $("#nilai4").on("mouseup touchend", function () {
-    kelasWarnaAbu(3);
-  });
-});
-
-//styling grafik nilai
-function kelasWarnaAbu(i) {
-  $(".label-slider")
-    .eq(i)
-    .removeClass("mdl-color-text--blue")
-    .addClass("mdl-color-text--grey-400");
-}
-
-function kelasWarnaBiru(i) {
-  $(".label-slider")
-    .eq(i)
-    .removeClass("mdl-color-text--grey-400")
-    .addClass("mdl-color-text--blue");
-}
-
-//Utility
-
-//tombol close
+/*
+ * Fitur Tambahan
+ * Membuat tombol close pada control object
+ * (http://fabricjs.com/custom-control-render)
+ */
 var img = document.createElement("img");
 img.src = "../imgs/close.png";
 
@@ -401,16 +476,13 @@ function renderIcon(ctx, left, top, styleOverride, fabricObject) {
   ctx.restore();
 }
 
-//keyboard delete
-// var canvasWrapper = document.getElementById("canvasWraper");
-// canvasWrapper.tabIndex = 1000;
-// canvasWrapper.addEventListener("keydown", deleteObjectKeyboard(), false);
-
-// function deleteObjectKeyboard() {
-//   document.onkeydown = function (e) {
-//     if ("Delete" === e.key) {
-//       canvas.remove(canvas.getActiveObject());
-//       canvas.requestRenderAll();
-//     }
-//   };
-// }
+/*
+ * Fungsi cancelForm
+ * Berfungsi untuk mencegah form disubmit
+ * karena pada aplikasi ini pemorosesan input dilakukan
+ * saat event onchange dan atau oninput
+ */
+function cancelForm(e) {
+  e.preventDefault();
+  return false;
+}
